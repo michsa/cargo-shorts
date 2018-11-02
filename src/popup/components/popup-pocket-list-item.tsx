c9098c68e1dd9b3fefeb381bbb03f9217b04eb84
Michelle Saad <git@michsa.me>
2018-11-02 16:44:58 -0400

    add 'current' flag, handler interface to pocket list

--- src/popup/components/popup-pocket-list-item.tsx
+++ src/popup/components/popup-pocket-list-item.tsx
@@ -4,8 +4,8 @@ import * as emoji from 'node-emoji'
 
 interface Props {
   pocket: Pocket,
-  key: number,
-  isActive: boolean
+  isActive: boolean,
+  editHandler: (id: string) => void
 }
 
 const style = (color, isActive) => ({
@@ -13,16 +13,22 @@ const style = (color, isActive) => ({
   boxShadow: `inset 0 0 0 1px rgba(0,0,0,.15)`
 })
 
-const PopupPocketListItem = ({pocket, isActive}: Props) => (
-  <div id="pocket-list-item-details" style={style(pocket.color, isActive)}>
-    <span id="pocket-icon">{
-      emoji.hasEmoji(pocket.icon) 
-      ? emoji.get(pocket.icon) 
-      : emoji.random().emoji
-    }</span>
-    <span id="pocket-name">{pocket.name}</span>
-    <span id="pocket-count">{pocket.tabs.length}</span>
-  </div>
-)
+const PopupPocketListItem = ({pocket, isActive, editHandler}: Props) => {
+  const editPocket = () => editHandler(pocket.id)
+  return (
+    <li id="pocket-list-item" key={pocket.id}>
+      <div id="pocket-list-item-details" style={style(pocket.color, isActive)}>
+        <span id="pocket-icon">{
+          emoji.hasEmoji(pocket.icon) 
+          ? emoji.get(pocket.icon) 
+          : emoji.random().emoji
+        }</span>
+        <span id="pocket-name">{pocket.name}</span>
+        <span id="pocket-count">{pocket.tabs.length}</span>
+      </div>
+      <i id="edit-pocket" className="ui icon pencil" onClick={editPocket} />
+    </li>
+  )
+}
 
 export default PopupPocketListItem
--- src/popup/components/popup-pocket-list.tsx
+++ src/popup/components/popup-pocket-list.tsx
@@ -1,5 +1,5 @@
-import { State } from '../../types'
-import { orderedPocketSelector } from '../../selectors'
+import { State, Pocket, Tab } from '../../types'
+import { orderedPocketSelector, currentPocketIdSelector } from '../../selectors'
 import { connect } from 'react-redux'
 import { routeNewPocket, routeEditPocket } from '../../actions'
 
@@ -7,30 +7,46 @@ import * as React from 'react'
 import PopupPocketListItem from './popup-pocket-list-item'
 import TabInfo from './tab-info'
 
+interface Props {
+  pockets: Pocket[]
+  currentPocket: string, // pocket id
+  tab: Tab
+}
+
 const mapStateToProps = (state: State) => ({
     pockets: orderedPocketSelector(state),
+    currentPocket: currentPocketIdSelector(state),
     tab: state.tabs.current
-})
+} as Props)
+
+interface Handlers {
+  handlers: {
+    newPocket: () => void
+    editPocket: (id: string) => void
+  }
+}
 
 const mapDispatchToProps = dispatch => ({
   handlers: {
-    newPocket: id => {
+    newPocket: () => {
       dispatch(routeNewPocket())
     },
-    editPocket: id => {
+    editPocket: (id: string) => {
       dispatch(routeEditPocket(id))
     }
   }
 })
 
-const PopupPocketList = ({pockets, handlers}) => (
+const PopupPocketList = ({pockets, currentPocket, tab, handlers}: (Props & Handlers)) => (
   <ul id="pocket-list">
     <li><TabInfo/></li>
-    {pockets.map((pocket, index) => 
-      <li id="pocket-list-item">
-        <PopupPocketListItem pocket={pocket} key={index} isActive={index % 2 === 0} />
-        <i id="edit-pocket" className="ui icon pencil" onClick={handlers.editPocket} />
-      </li>
+    {pockets.map((pocket) => 
+      <PopupPocketListItem 
+          pocket={pocket} 
+          isActive={currentPocket === pocket.id} 
+          editHandler={handlers.editPocket} 
+          key={pocket.id}
+      />
     )}
     <li onClick={handlers.newPocket}>+ New Pocket</li>
   </ul>
