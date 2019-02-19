ededce7c9afe6b52aa5a59b6f688da6ac20a6fda
Michelle Saad <git@michsa.me>
2019-02-18 20:40:46 -0500

    persist yay

--- src/background.tsx
+++ src/background.tsx
@@ -1,20 +1,3 @@
-// import browser from 'webextension-polyfill'
-// window.browser = browser
-// import { requestCurrentTabInfo } from './redux/actions'
 import backgroundStore from './redux/store'
 
-console.log('background.tsx')
-console.log(backgroundStore.getState())
-
-/*
-async function loadStorageData() {
-  const data = await browser.storage.sync.get()
-
-  backgroundStore.dispatch({
-    type: 'LOAD_STORAGE_DATA',
-    data
-  })
-}
-
-loadStorageData()
-*/
+console.log(backgroundStore)
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,6 @@
 import { applyMiddleware, createStore } from 'redux'
+import { persistReducer, persistStore } from 'redux-persist'
+import { syncStorage } from 'redux-persist-webextension-storage'
 import thunk from 'redux-thunk'
 import { createBackgroundStore } from 'redux-webext'
 import { getType } from 'typesafe-actions'
@@ -7,13 +9,22 @@ import * as actions from './actions'
 import * as ui from './actions/ui'
 import reducer from './reducers'
 
-const store = createStore(
-  reducer,
-  applyMiddleware(thunk)
-)
+const storageConfig = {
+  key: 'syncStorage',
+  storage: syncStorage,
+}
+
+const persistedStore = () => {
+  const store = createStore(
+    persistReducer(storageConfig, reducer),
+    applyMiddleware(thunk)
+  )
+  const persistor = persistStore(store)
+  return { store, persistor }
+}
 
 export default createBackgroundStore({
-  store,
+  store: persistedStore().store,
   actions: {
     [getType(ui.requestCurrentTabInfo)]: actions.getCurrentTabInfo,
     [getType(ui.newTab)]: actions.newTab,
