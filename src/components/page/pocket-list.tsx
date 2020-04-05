ca6d476949601c43cb2c3f135d66c14201618b0f
Michelle Saad <git@michsa.me>
2020-04-05 17:26:12 -0400

    clean up page/pocket-list

--- src/components/page/pocket-list.tsx
+++ src/components/page/pocket-list.tsx
@@ -1,11 +1,9 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
 import { useState } from 'react'
-import { DropResult } from 'react-beautiful-dnd'
 import { connect } from 'react-redux'
 import { XMasonry, XBlock } from 'react-xmasonry'
 
-import { movePocket, moveTab } from '../../redux/actions/ui'
 import { getOrderedPockets } from '../../redux/selectors'
 import { Pocket, State } from '../../types'
 
@@ -15,45 +13,25 @@ interface Props {
   pockets: Pocket[]
 }
 
-interface Handlers {
-  onDragEnd: (result: DropResult) => void
-}
-
 const mapStateToProps = (state: State) =>
-  ({
-    pockets: getOrderedPockets(state)
-  } as Props)
+  ({ pockets: getOrderedPockets(state) } as Props)
 
-const mapDispatchToProps = {
-  onDragEnd: (result: DropResult) =>
-    result.destination &&
-    (result.type === 'COLUMN'
-      ? movePocket({
-          start: result.source.index,
-          end: result.destination.index
-        })
-      : moveTab({
-          tabId: result.draggableId,
-          pocketId: result.destination.droppableId,
-          position: result.destination.index
-        }))
-} as Handlers
-
-const PocketList = ({ pockets, onDragEnd }: Props & Handlers) => {
+const PocketList = ({ pockets }: Props) => {
   const [grid, setGrid] = useState()
   return (
-      <XMasonry ref={setGrid} smartUpdate={false} targetBlockWidth={400} maxColumns={4}>
-        {pockets.map((pocket) => (
-          <XBlock key={pocket.id}>
-          <PocketListItem
-            pocket={pocket}
-            grid={grid}
-            handleEdit={x => x}
-          />
-          </XBlock>
-        ))}
-      </XMasonry>
+    <XMasonry
+      ref={setGrid}
+      smartUpdate={false}
+      targetBlockWidth={400}
+      maxColumns={4}
+    >
+      {pockets.map(pocket => (
+        <XBlock key={pocket.id}>
+          <PocketListItem pocket={pocket} grid={grid} handleEdit={x => x} />
+        </XBlock>
+      ))}
+    </XMasonry>
   )
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(PocketList)
+export default connect(mapStateToProps)(PocketList)
