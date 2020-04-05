008c9d838b9261cc45b89be20e39c3350bb8322a
Michelle Saad <git@michsa.me>
2020-04-05 19:37:44 -0400

    use state in pocket summary

--- src/components/page/pocket-summary.tsx
+++ src/components/page/pocket-summary.tsx
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
+import { useState } from 'react'
 import { connect } from 'react-redux'
 import { ReactSortable } from 'react-sortablejs'
 
@@ -20,40 +21,42 @@ const mapStateToProps = (state: State) =>
 
 const mapDispatchToProps = { reorderPockets } as Handlers
 
-const PocketIcons = ({ pockets, reorderPockets }: Props & Handlers) => (
-  <Flex className="pocket-summary" center>
-    <ReactSortable
-      group="tabs"
-      list={pockets}
-      animation={200}
-      setList={(x: Pocket[] | { payload: Pocket[] }) => {
-        if ('payload' in x) x = x.payload
-        console.log(x.map(p => p.id))
-        reorderPockets(x.map(p => p.id))
-      }}
-      // onChange={() => grid && grid.update()}
-      css={{ display: 'flex' }}
-    >
-      {pockets.map(pocket => (
-        <Flex
-          center
-          className="pocket-summary-item"
-          gap={8}
-          css={{
-            backgroundColor: pocket.color,
-            padding: '5px 7px',
-            borderRadius: 4,
-            margin: 4,
-            boxShadow: '0 1px 3px rgba(0, 0, 0, 0.2)'
-          }}
-          key={pocket.id}
-        >
-          <PocketIcon icon={pocket.icon} />
-          <PocketCount count={pocket.tabs.length} />
-        </Flex>
-      ))}
-    </ReactSortable>
-  </Flex>
-)
+const PocketIcons = ({ pockets, reorderPockets }: Props & Handlers) => {
+  const [state, setState] = useState(pockets)
+  return (
+    <Flex className="pocket-summary" center>
+      <ReactSortable
+        group="tabs"
+        list={state}
+        animation={200}
+        setList={(x: Pocket[]) => {
+          setState(x)
+          reorderPockets(x.map(p => p.id))
+        }}
+        // onChange={() => grid && grid.update()}
+        css={{ display: 'flex' }}
+      >
+        {state.map(pocket => (
+          <Flex
+            center
+            className="pocket-summary-item"
+            gap={8}
+            css={{
+              backgroundColor: pocket.color,
+              padding: '5px 7px',
+              borderRadius: 4,
+              margin: 4,
+              boxShadow: '0 1px 3px rgba(0, 0, 0, 0.2)'
+            }}
+            key={pocket.id}
+          >
+            <PocketIcon icon={pocket.icon} />
+            <PocketCount count={pocket.tabs.length} />
+          </Flex>
+        ))}
+      </ReactSortable>
+    </Flex>
+  )
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(PocketIcons)
